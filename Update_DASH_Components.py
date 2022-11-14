826e7eb8b2d77453bdbb3e40a53a60f157798bf6
Miguel Numa <82172636+mnumam@users.noreply.github.com>
2022-11-14 15:24:41 -0700

    Update Update_DASH_Components.py
    Updated command to import html and dcc

--- Update_DASH_Components.py
+++ Update_DASH_Components.py
@@ -8,8 +8,7 @@ import os
 
 # DASH
 import dash
-import dash_bootstrap_components as dbc
-import dash_html_components as html
+from dash import html, dcc
 from dash.dependencies import Input, Output, State
 
 def Update_DASH_Components(path_2_pngs_roi, path_2_pngs_amap):
